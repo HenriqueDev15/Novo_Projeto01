66c5cb74153693ca1e4763363b20c97800937b4a
HenriqueDev15 <oliveira.vasconcelos@escolar.ifrn.edu.br>
2025-01-24 11:55:54 -0300

    Camera.morte

---
+++ Projeto 2D Saulo/Assets/Scenes/Morreu.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Morreu : MonoBehaviour
+{
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
